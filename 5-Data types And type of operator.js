d076baec367d0ca6b6083037c9acdbcf4b718aaf
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-02 16:37:27 +0200

    Add files via upload

---
+++ 5-Data types And type of operator.js
@@ -0,0 +1,25 @@
+/*
+data types
+---string
+---number
+---Oject => array
+---Object
+---Boolean
+*/
+
+
+console.log("Ammar Ba");
+//string
+console.log(typeof ("Ammar Ba"));
+//number
+console.log(typeof 5000);
+console.log(typeof 5000.98);
+//array / object
+console.log(typeof [10 ,50 ,55]);
+console.log(typeof ["Am" ,"Om" ,"Mo"]);
+console.log(typeof {Nanme :'Am',Age : 17,Country : 'Sy'});
+//Boolean
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
