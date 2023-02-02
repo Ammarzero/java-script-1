d4a148b9de90e38b8c75585a2ada93d6068743c5
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-02 13:51:38 +0200

    Add files via upload

---
+++ 3-method and styling.js
@@ -0,0 +1,7 @@
+console.log("log");
+console.error("error");
+// to make table with values  and their indexes
+console.table(["ahmed","mostafa","Ali"]);
+//styling the log !
+console.log("hello from js"),"color :red; font_size:80px"
+
