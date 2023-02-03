84f32d8e3eefcb98759684c2c81f60b29368913a
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-03 20:08:38 +0200

    Add files via upload

---
+++ 4-2Assingment/4-2Assingment.js
@@ -0,0 +1,19 @@
+
+  document.write("<h1>Ammar</h1>")
+  document.querySelector("h1").style.color = "red";
+  document.querySelector("h1").style.textAlign = "center";
+  document.querySelector("h1").style.fontsize = "200px";
+  document.querySelector("h1").style.fontFamily = "arial";
+  document.querySelector("h1").style.fontWeight = "bold";
+
+
+
+
+
+
+    /*color: red;
+font-size: 200px;
+font-weight: bold;
+text-align: center;
+font-family: Arial
+   */
