95869678c5b435cc161b5360fa1eac3cbbee70f0
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-04 11:15:55 +0200

    Add files via upload

---
+++ 6-Variables/6-main.js
@@ -0,0 +1,24 @@
+/*
+ -Variables Intro
+   -What is variable ?
+   -Why we use Variables ?
+   -Declare a Variable And Use 
+   -Syntax ( Keyword / variable name / assingment operator )
+   -Variables Without Var
+   -Multibles Variables in The Same Line
+   -Id And Global Variable
+   -Loosley Tybed Vs Strongly Tybed
+   */
+
+
+   // the declare must be at first !! to code work..
+var user = "Yamen";
+var age = 20;
+
+   console.log("user");
+   console.log("user");
+   console.log("user");
+   console.log("age");
+   console.log("hi");
+
+   hi.innerHTML = "welcome";
