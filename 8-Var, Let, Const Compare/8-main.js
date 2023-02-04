258425935153d3a74d882e4e49f54bc6b9f367c7
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-04 11:55:40 +0200

    Add files via upload

---
+++ 8-Var, Let, Const Compare/8-main.js
@@ -0,0 +1,23 @@
+/*
+ Var
+  - Redeclare (Yes)
+  -Accsess Before Declare (Undefined)
+  -Variable Scope Drama [Added To Window] ()
+  -Block Or Function Scope
+  
+ Let
+  -Redclare (No => Error)
+  -Accsess Before Declare (Error)
+  -Variable Scope Drama ()
+  -Block Or Function Scope
+
+ Const
+  -Redclare (No => Error)
+  -Accsess Before Declare (Error)
+  -Variable Scope Drama ()
+  -Block Or Function Scope
+  */
+
+let abvxg = 1
+
+console.log(abvxg);  
