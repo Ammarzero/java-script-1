e79a6c31ba90c1ee47c2b3aa14e793ab5e7c508b
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-04 11:30:38 +0200

    Add files via upload

---
+++ 7-Identifiers Name Convntions And Rules/7-main.js
@@ -0,0 +1,15 @@
+/*
+ Identifiers
+  -Name Conventions And Rules
+  -Reserved Words
+  */
+
+  var user = "Ammar";
+
+  console.log(user);
+  /* Rules For Writing :
+   -No Distance 
+   -Dont Start By (Number ,.......)
+   -There Are Some Word Dont Be Allowed To Used  
+  IF You Want Write Two Words  You Can Start The 
+  Second By Capital Letter. 
