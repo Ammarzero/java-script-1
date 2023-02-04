50a3f19e3b234088ca17fe0051734ba6e5c3c8d2
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-04 13:10:27 +0200

    Add files via upload

---
+++ 11-Template Literals Template Strings/11-main.js
@@ -0,0 +1,15 @@
+/*
+Template Literals (Template Strings)
+*/
+
+let a = "I Love";
+let b = "JavaScript";
+let c = "Courses";
+let d = "Very Much !!";
+
+console.log(a + " \"\" " + b +
+ " \n" + c +" " + d)
+ 
+
+ console.log(`${a} "" '' \\ ${b} 
+ ${c} ${d}`);
