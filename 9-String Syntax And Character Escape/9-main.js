6fd33492592125dc649aef16726508df450f3a1c
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-04 12:16:08 +0200

    Add files via upload

---
+++ 9-String Syntax And Character Escape/9-main.js
@@ -0,0 +1,14 @@
+/*
+String Syntax + Escape Sequences
+ \ Escape + Line Continue
+ \n
+ */
+
+ console.log('Ammar Al "Baour"');
+ console.log('Ammar Al \'Baour\'');
+ console.log("Ammar Al 'Baour'");
+ console.log("Ammar \\ Al \"Baour\"");
+ console.log( "Amamr \
+ AL \
+ Baour");
+ console.log('Ammar \nAl \nBaour');
