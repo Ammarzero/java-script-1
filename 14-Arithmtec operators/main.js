e86f9557f27c52e09dc7765ab744a50ca8a1fb77
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-05 17:57:36 +0200

    Add files via upload

---
+++ 14-Arithmtec operators/main.js
@@ -0,0 +1,31 @@
+/*
+Arithmetic Operators
++ Addition
+- Subtraction
++ Multiblication
+/ Division
+++ Exponentiation (ES7)
+% Modulus (Division Remainder)
+++ increment [ Post / Pre]
+== Decrement [ Post / Pre]
+*/
+
+console.log(10 + 20);
+console.log(10 + "Yamen");
+
+console.log(50 - 60);
+console.log(50 - "Yamen"); //NaN
+
+console.log(typeof NaN);
+
+console.log(100 * 2);
+console.log(100 * -2);
+
+console.log(100 / 2);
+console.log(100 / 3);
+
+console.log(5 ** 3);
+console.log(5 * 5 * 5);
+
+console.log(100 % 2);
+console.log(101 % 2);// Remove 1
