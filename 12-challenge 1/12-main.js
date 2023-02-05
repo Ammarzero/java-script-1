54f692eff16e9f6baa5fb926b0f367bca8a85531
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-05 14:40:32 +0200

    Add files via upload

---
+++ 12-challenge 1/12-main.js
@@ -0,0 +1,9 @@
+var TitleMe = "Java Script";
+var DiscriptionAbout = "Learning Vs Code";
+var DateOn = "25 / 10";
+document.write(`
+<div id ="Card">
+    <h3>${TitleMe}</h3>
+    <p>${DiscriptionAbout}</p> 
+    <span>${DateOn}</span>   
+</div>`.repeat(4));
