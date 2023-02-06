f29615b84f850ad5396766fcdb32302b7623a7c3
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-06 17:38:59 +0200

    Add files via upload

---
+++ 20-Number/main.js
@@ -0,0 +1,21 @@
+/*
+Number
+- Double Precision
+- Syntactic Sugar "_"
+- e
+- **
+- With Decimal
+- Number + BigInt
+- Number Min Value
+- Number Max Value
+*/
+
+console.log(1000000);
+console.log(1_000_000);
+console.log(1e6);
+console.log(10 * 10 * 10 * 10 * 10 * 10);
+console.log(1000000.000)
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_VALUE + 32423423423423);
