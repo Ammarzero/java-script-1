0b837199318a9ed0fdf194a3a4464b33b4e2417f
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-06 14:44:27 +0200

    Add files via upload

---
+++ 15-Unary Plus And Negation Operators/main.js
@@ -0,0 +1,36 @@
+/*
+-  + Unary Plus [Return Number If Its Not Number]
+- -Unary Negation [Return Number If Its Not Number + Negation It]
+Tests
+- Normal Number
+- String Number
+- String Negativr Number
+- String Text
+- Float
+- Hexadecimal Number System => OxFF
+- null
+- false
+- true
+*/
+
+console.log(+100);
+console.log(+"100");
+console.log(-"-100");
+console.log(+"Ammar");
+console.log(+"51.6")
+console.log(+0xff);
+console.log(+null);
+console.log(+true);
+console.log(+false);
+
+console.log(-100);
+console.log(-"100");
+console.log(-"-100");
+console.log(-"Ammar");
+console.log(-"51.6")
+console.log(-0xff);
+console.log(-null);
+console.log(-true);
+console.log(-false);
+
+console.log(Number("100"));
