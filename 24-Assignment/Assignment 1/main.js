0541f720a3125ed2c526b00a50f77a5f8d41e6f9
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-07 09:37:34 +0200

    Add files via upload

---
+++ 24-Assignment/Assignment 1/main.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(Math.pow(10, 5)); // 100000
+console.log(10 * 10 * 10 * 10 * 10); // 100000
+console.log(100 * 1000); // 100000
+console.log(1_00_000); // 100000
+console.log(1e5); // 100000
+console.log(10 ** 5); // 100000
+console.log(-(-100000)); // 100000
+console.log(-(-1e5)); // 100000
+console.log(2e4 * 5); // 100000
+console.log(Math.floor(100000.76)); // 100000
