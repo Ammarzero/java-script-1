48a2139cf4329c7d6dde3dcae69f62bb3b9265be
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-02-07 08:48:07 +0200

    Add files via upload

---
+++ 22-Math Object/main.js
@@ -0,0 +1,27 @@
+/*
+Math Object
+- round ()
+- ceil ()
+- floor()
+- min ()
+- max ()
+- pow ()
+- random ()
+- trunc() [ES6]
+*/
+
+console.log(Math.round(79.5));
+console.log(Math.round(77.1));
+
+console.log(Math.ceil(43.1));
+console.log(Math.floor(77.9));
+
+console.log(Math.min(10, 20, -87, 51, 123, 10));
+console.log(Math.max(10, 20, -87, 51, 123, 10));
+
+console.log(Math.pow(4, 10));
+
+console.log(Math.random());
+
+console.log(Math.trunc(22.9));
+
